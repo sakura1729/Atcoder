b2270198df6e0246e868e2e0c7292fb232335170
sakura1729 <daidai161019@gmail.com>
2023-05-27 02:43:13 +0900

    e

--- test2.cpp
+++ test2.cpp
@@ -1,7 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    cout << "Hello World" << endl << n << endl;
+int main()
+{
+    cout << "Hello World" << endl;
 }
